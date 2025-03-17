07d3a993583a4fb00c48c7dd840d8953ccbbc2fc
Jateloz <164486637+Jateloz@users.noreply.github.com>
2025-03-17 13:32:10 +0300

    Changes

--- src/main/java/com/example/jatelobank/ActivityWindow/Reports/ReportsController.java
+++ src/main/java/com/example/jatelobank/ActivityWindow/Reports/ReportsController.java
@@ -4,8 +4,6 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
 import javafx.scene.layout.VBox;
 
 import java.net.URL;
@@ -16,18 +14,20 @@ import java.util.logging.Logger;
 
 public class ReportsController implements Initializable {
     public VBox mainContent;
-    public TableView reportTable;
-    public TableColumn columnReportName;
-    public TableColumn columnDate;
-    public TableColumn columnStatus;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        //load the transaction report on   opening the reports page
+        Parent root = null;
+        try {
+            root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/Fxml/Activity/Reports/TransactionReport.fxml")));
 
-    }
+        }catch (Exception e){
+            Logger.getLogger(ModuleLayer.Controller.class.getName()).log(Level.SEVERE,null,e);
 
-    public void handleDownloadReport(ActionEvent event) {
-
+            e.printStackTrace();
+        }
+        mainContent.getChildren().add(root);
     }
 
     public void showTransactionReport(ActionEvent event) {
@@ -44,14 +44,41 @@ public class ReportsController implements Initializable {
     }
 
     public void showInvestmentReport(ActionEvent event) {
+        Parent root = null;
+        try {
+            root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/Fxml/Activity/Reports/InvestmentsReport.fxml")));
 
+        }catch (Exception e){
+            Logger.getLogger(ModuleLayer.Controller.class.getName()).log(Level.SEVERE,null,e);
+
+            e.printStackTrace();
+        }
+        mainContent.getChildren().add(root);
     }
 
     public void showUserAccountReport(ActionEvent event) {
+        Parent root = null;
+        try {
+            root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/Fxml/Activity/Reports/UserAccountsReport.fxml")));
 
+        }catch (Exception e){
+            Logger.getLogger(ModuleLayer.Controller.class.getName()).log(Level.SEVERE,null,e);
+
+            e.printStackTrace();
+        }
+        mainContent.getChildren().add(root);
     }
 
     public void showTransfersReport(ActionEvent event) {
+        Parent root = null;
+        try {
+            root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/Fxml/Activity/Reports/TransactionReport.fxml")));
 
+        }catch (Exception e){
+            Logger.getLogger(ModuleLayer.Controller.class.getName()).log(Level.SEVERE,null,e);
+
+            e.printStackTrace();
+        }
+        mainContent.getChildren().add(root);
     }
 }
